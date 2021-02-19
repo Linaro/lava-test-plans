Linaro/lava-test-plans

commit 806413f14ea7be9c7be1b773d5de04e2007f2b11

submit_for_testing: qa_server_project need a valid string
Add a check so the qa_server_project have a valid string, a valid string
can't contain '/'.

Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

submit_for_testing.py
@@ -286,6 +286,10 @@ def main():
     exit_code = 0
 
     output_path = os.path.join(script_dirname, "tmp")
+    if "/" in args.qa_server_project:
+        logger.error("--qa-server-project can not contain of a slash in the name")
+        sys.exit(1)
+
     if args.dryrun:
         if not os.path.exists(output_path):
             os.mkdir(output_path)

commit 80c306758d07f783a00d2a4f48fb2d34526fa9ce

submit_for_testing: qa_server_project TypeError
args.qa_server_project has to be set, otherwise when Python checks this
value a TypeError: argument of type 'NoneType' is not iterable will be
raised.

Fixes: 806413f14ea7 ("submit_for_testing: qa_server_project need a valid string")
Signed-off-by: Benjamin Copeland <ben.copeland@linaro.org>

submit_for_testing.py
@@ -286,9 +286,11 @@ def main():
     exit_code = 0
 
     output_path = os.path.join(script_dirname, "tmp")
-    if "/" in args.qa_server_project:
-        logger.error("--qa-server-project can not contain of a slash in the name")
-        sys.exit(1)
+
+    if args.qa_server_project:
+        if "/" in args.qa_server_project:
+            logger.error("--qa-server-project can not contain of a slash in the name")
+            sys.exit(1)
 
     if args.dryrun:
         if not os.path.exists(output_path):