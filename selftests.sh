cde141ad2b7e100d5e4ae427d5190c520df1ec7a
Yongqin Liu <yongqin.liu@linaro.org>
2022-02-15 21:58:44 +0800

    android: add testcase templates for x15 and hikey960
    with the docker method used for lava jobs.
    needs to specify the --testplan-path to testcases/android,
    and --testplan-device-path to devices/android/,
    for the submit_for_testing.py script to be run.
    
    Here created links for template-xxxx.yaml files, because
    the current build configs use filename like this.
    Will remove these template-xxx.yaml files when the build configs
    side changed
    
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- selftests.sh
+++ selftests.sh
@@ -6,22 +6,52 @@ function run_device_selftest()
     echo $?
 }
 
-exit_code=0
-for DEVICE_TYPE in ./devices/*
-do
-    for TEST in ./testcases/*.yaml
-    do
-        if [ -d $DEVICE_TYPE ]
-        then
-            for NESTED_DEVICE_TYPE in $DEVICE_TYPE/*
-            do
-                device_exit_code=$(run_device_selftest $(basename $DEVICE_TYPE)/$(basename $NESTED_DEVICE_TYPE) $(basename $TEST))
-                if [ $device_exit_code -gt $exit_code ]; then exit_code=$device_exit_code; fi
-            done
-        else
-            device_exit_code=$(run_device_selftest $(basename $DEVICE_TYPE) $(basename $TEST))
-            if [ $device_exit_code -gt $exit_code ]; then exit_code=$device_exit_code; fi
-        fi
+function test_for_android_templates(){
+    local exit_code=0
+    for F_DEVICE_TYPE in ./devices/android/*; do
+        DEVICE_TYPE=$(basename "${F_DEVICE_TYPE}")
+        for F_TESTCASE in ./testcases/android/*.yaml; do
+            TEST=$(basename ${F_TESTCASE})
+            ./submit_for_testing.py \
+                --testplan-path ./testcases/android/ \
+                --testplan-device-path ./devices/android/ \
+                --test-case ${TEST} \
+                --variables variables-android.ini \
+                --device-type ${DEVICE_TYPE} \
+                --dry-run \
+
+           [ $? -ne 0 ] && exit_code=$?
+        done
     done
-done
-exit $exit_code
+    return ${exit_code}
+}
+
+function test_for_lts_templates(){
+    local exit_code=0
+    for DEVICE_TYPE in ./devices/*
+    do
+        if [ "X$(basename ${DEVICE_TYPE})" = "Xandroid" ]; then
+            # test for android templates will be done separately
+            continue
+        fi
+        for TEST in ./testcases/*.yaml
+        do
+            if [ -d $DEVICE_TYPE ]
+            then
+                for NESTED_DEVICE_TYPE in $DEVICE_TYPE/*
+                do
+                    device_exit_code=$(run_device_selftest $(basename $DEVICE_TYPE)/$(basename $NESTED_DEVICE_TYPE) $(basename $TEST))
+                    if [ $device_exit_code -gt $exit_code ]; then exit_code=$device_exit_code; fi
+                done
+            else
+                device_exit_code=$(run_device_selftest $(basename $DEVICE_TYPE) $(basename $TEST))
+                if [ $device_exit_code -gt $exit_code ]; then exit_code=$device_exit_code; fi
+            fi
+        done
+    done
+
+    return $exit_code
+}
+
+test_for_lts_templates
+test_for_android_templates
