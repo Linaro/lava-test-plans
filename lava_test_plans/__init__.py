4b3372c07145cae6071305a8eb10a1d79a514e7a
Rémi Duraffort <remi.duraffort@linaro.org>
2022-08-10 14:39:43 +0200

    Move sources into lava_test_plans python module
    Signed-off-by: Rémi Duraffort <remi.duraffort@linaro.org>

---
+++ lava_test_plans/__init__.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# vim: set ts=4
+#
+# Copyright 2022-present Linaro Limited
+#
+# SPDX-License-Identifier: MIT
+
+"""
+Generate LAVA job definitions for different devices and tests
+"""
+
+__version__ = "3.0"
+
+from functools import lru_cache
+from pathlib import Path
+
+import jinja2
+
+
+BASE = Path(__file__).parent.resolve()
+
+
+@lru_cache(maxsize=None)
+def env():
+    return jinja2.Environment(
+        autoescape=False,
+        loader=jinja2.FileSystemLoader(
+            [BASE, BASE / "testplans", BASE / "testcases", BASE / "devices"],
+            followlinks=True,
+        ),
+        # undefined=jinja2.StrictUndefined,
+    )
+
+
+def render(template, context):
+    data = env().get_template(template).render(**context)
+    return "\n".join(l for l in data.split("\n") if l)
