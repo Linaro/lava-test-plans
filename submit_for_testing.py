5ea8573660bcd81b8ef53de1e9aa00de1b4b172e
Anders Roxell <anders.roxell@linaro.org>
2022-09-08 22:37:25 +0200

    submit_for_testing: fix, python in the py-script
    Rework so the python script submit_for_testing.py uses python and not
    sh.
    
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- submit_for_testing.py
+++ submit_for_testing.py
@@ -1,2 +1,8 @@
-#!/bin/sh
-exec python3 -m lava_test_plans $@
+#!/usr/bin/python3
+# Copyright 2022-present Linaro Limited
+#
+# SPDX-License-Identifier: MIT
+
+from lava_test_plans.__main__ import main
+
+main()
