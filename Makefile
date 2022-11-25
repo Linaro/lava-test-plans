4f57d507a9a2b795a38b3fd8fc2c78e6d6b4490c
Anders Roxell <anders.roxell@linaro.org>
2022-11-25 08:54:11 +0100

    github: test-plans-pipeline: use new unittests
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 all: typecheck test style flake8
 
 export PROJECT := lava_test_plans
+export NUM_WORKERS=$(shell nproc)
 
 include $(shell tuxpkg get-makefile)
--- test/test_lava_test_plans.py
+++ test/test_lava_test_plans.py
@@ -4,112 +4,118 @@ from unittest import TestCase
 import sys
 import glob
 import os
-from multiprocessing import Pool
+import pytest
 
 
-def call_lava_test_plan_testcases(param):
+# all tests all devices
+devices = [os.path.basename(d) for d in glob.glob("lava_test_plans/devices/*")]
+testcases = [os.path.basename(d) for d in glob.glob("lava_test_plans/testcases/*.yaml")]
+variable_input_file = "variables.ini"
+tests = []
+for device in devices:
+    for testcase in testcases:
+        tests.append((variable_input_file, device, testcase))
+
+
+@pytest.mark.parametrize("param", tests)
+def test_call_lava_test_plan_testcases(param):
     variable_input_file, device, testcase = param
     sys.argv = ["lava_test_plans", "--dry-run", "--variables", variable_input_file, "--device-type", device, "--test-case", testcase]
-    return main() == 0
+    assert main() == 0
 
 
-def call_lava_test_plan_testplans(param):
+# armnn tests
+armnn_devices = ['hi960-hikey', 'dragonboard-845c', 'synquacer', 'stm32mp157c-dk2', 'juno']
+assert len(armnn_devices) > 0
+armnn_testplans = ['armnn', 'armnn-benchmarking']
+assert len(armnn_testplans) > 0
+armnn_variable_input_file = "projects/armnn/variables.yaml"
+tests = []
+for device in armnn_devices:
+    for testplan in armnn_testplans:
+        tests.append((armnn_variable_input_file, device, testplan))
+
+
+@pytest.mark.parametrize("param", tests)
+def test_call_lava_test_plan_testplans_armnn(param):
     variable_input_file, device, testplan = param
     sys.argv = ["lava_test_plans", "--dry-run", "--variables", variable_input_file, "--device-type", device, "--test-plan", testplan, "--test-lava-validity"]
-    return main() == 0
+    assert main() == 0
 
 
-def call_lava_test_plan_testcases_project(param):
-    variable_input_file, device, testcase, project_device_path = param
-    sys.argv = ["lava_test_plans", "--dry-run", "--variables", variable_input_file, "--testplan-device-path", project_device_path, "--device-type", device, "--test-case", testcase, "--test-lava-validity"]
-    return main() == 0
+# lkft tests
+lkft_project_device_path = "lava_test_plans/projects/lkft/devices"
+lkft_devices = [os.path.basename(d) for d in glob.glob("lava_test_plans/projects/lkft/devices/*")]
+assert len(lkft_devices) > 0
+lkft_testplans = ['lkft-full', 'lkft-kselftest', 'lkft-ltp', 'lkft-rt', 'lkft-sanity']
+assert len(lkft_testplans) > 0
+lkft_variable_input_file = "projects/lkft/variables.ini"
+tests = []
+for device in lkft_devices:
+    for testplan in lkft_testplans:
+        tests.append((lkft_variable_input_file, device, testplan, lkft_project_device_path))
 
 
-def call_lava_test_plan_testplans_project(param):
+@pytest.mark.parametrize("param", tests)
+def test_call_lava_test_plan_testplans_project_lkft(param):
     variable_input_file, device, testplan, project_device_path = param
     sys.argv = ["lava_test_plans", "--dry-run", "--variables", variable_input_file, "--testplan-device-path", project_device_path, "--device-type", device, "--test-plan", testplan, "--test-lava-validity"]
-    return main() == 0
+    assert main() == 0
 
 
-class TestLavaTestPlans(TestCase):
+# lt-qcom tests
+lt_qcom_project_device_path = "lava_test_plans/projects/lt-qcom/devices"
+lt_qcom_devices = [os.path.basename(d) for d in glob.glob("lava_test_plans/projects/lt-qcom/devices/*")]
+assert len(lt_qcom_devices) > 0
+lt_qcom_testplans = ['lt-qcom/kernel']
+assert len(lt_qcom_testplans) > 0
+lt_qcom_variable_input_file = "projects/lt-qcom/variables.yaml"
+tests = []
+for device in lt_qcom_devices:
+    for testplan in lt_qcom_testplans:
+        tests.append((lt_qcom_variable_input_file, device, testplan, lt_qcom_project_device_path))
 
-    def test_testcases_without_project(self):
-        devices = [os.path.basename(d) for d in glob.glob("lava_test_plans/devices/*")]
-        testcases = [os.path.basename(d) for d in glob.glob("lava_test_plans/testcases/*.yaml")]
-        variable_input_file = "variables.ini"
-        with Pool() as p:
-            for device in devices:
-                tests = [(variable_input_file, device, testcase) for testcase in testcases]
-                results = p.map(call_lava_test_plan_testcases, tests)
-                self.assertTrue(all(results))
-        self.assertTrue(True)
+@pytest.mark.parametrize("param", tests)
+def test_call_lava_test_plan_testplans_project_lt_qcom(param):
+    variable_input_file, device, testplan, project_device_path = param
+    sys.argv = ["lava_test_plans", "--dry-run", "--variables", variable_input_file, "--testplan-device-path", project_device_path, "--device-type", device, "--test-plan", testplan, "--test-lava-validity"]
+    assert main() == 0
 
-    def test_testplan_with_project_armnn(self):
-        devices = [os.path.basename(d) for d in glob.glob("projects/armnn/devices")]
-        testplans = ['armnn', 'armnn-benchmarking']
-        variable_input_file = "projects/armnn/variables.yaml"
-        with Pool() as p:
-            for device in devices:
-                tests = [(variable_input_file, device, testplan) for testplan in testplans]
-                results = p.map(call_lava_test_plan_testplans, tests)
-                self.assertTrue(all(results))
-        self.assertTrue(True)
 
-    def test_testcases_with_project_lkft(self):
-        project_device_path = "lava_test_plans/projects/lkft/devices"
-        devices = [os.path.basename(d) for d in glob.glob("projects/lkft/devices")]
-        testcases = [os.path.basename(d) for d in glob.glob("lava_test_plans/testcases/*.yaml")]
-        variable_input_file = "projects/lkft/variables.ini"
-        with Pool() as p:
-            for device in devices:
-                tests = [(variable_input_file, device, testcase, project_device_path) for testcase in testcases]
-                results = p.map(call_lava_test_plan_testcases_project, tests)
-                self.assertTrue(all(results))
-        self.assertTrue(True)
+# tf-a tests
+tf_a_project_device_path = "lava_test_plans/projects/tf-a/devices"
+tf_a_devices = [os.path.basename(d) for d in glob.glob("lava_test_plans/projects/tf-a/devices/*")]
+assert len(tf_a_devices) > 0
+tf_a_testplans = ['tf-a']
+assert len(tf_a_testplans) > 0
+tf_a_variable_input_file = "projects/tf-a/variables.yaml"
+tests = []
+for device in tf_a_devices:
+    for testplan in tf_a_testplans:
+        tests.append((tf_a_variable_input_file, device, testplan, tf_a_project_device_path))
 
-    def test_testplan_with_project_lkft(self):
-        project_device_path = "lava_test_plans/projects/lkft/devices"
-        devices = [os.path.basename(d) for d in glob.glob("projects//devices")]
-        testplans = ['lkft-full', 'lkft-kselftest', 'lkft-ltp', 'lkft-rt', 'lkft-sanity']
-        variable_input_file = "projects/lkft/variables.ini"
-        with Pool() as p:
-            for device in devices:
-                tests = [(variable_input_file, device, testplan, project_device_path) for testplan in testplans]
-                results = p.map(call_lava_test_plan_testplans_project, tests)
-                self.assertTrue(all(results))
-        self.assertTrue(True)
 
-    def test_testplan_with_project_lt_qcom(self):
-        project_device_path = "lava_test_plans/projects/lt-qcom/devices"
-        devices = [os.path.basename(d) for d in glob.glob("projects/lt-qcom/devices")]
-        testplans = ['lt-qcom/distro', 'lt-qcom/kernel']
-        variable_input_file = "projects/lt-qcom/variables.ini"
-        with Pool() as p:
-            for device in devices:
-                tests = [(variable_input_file, device, testplan, project_device_path) for testplan in testplans]
-                results = p.map(call_lava_test_plan_testplans_project, tests)
-                self.assertTrue(all(results))
-        self.assertTrue(True)
+@pytest.mark.parametrize("param", tests)
+def test_call_lava_test_plan_testplans_project_tf_a(param):
+    variable_input_file, device, testplan, project_device_path = param
+    sys.argv = ["lava_test_plans", "--dry-run", "--variables", variable_input_file, "--testplan-device-path", project_device_path, "--device-type", device, "--test-plan", testplan, "--test-lava-validity"]
+    assert main() == 0
 
-    def test_testplan_with_project_tf_a(self):
-        project_device_path = "lava_test_plans/projects/tf-a/devices"
-        devices = [os.path.basename(d) for d in glob.glob("projects/tf-a/devices")]
-        testplans = ['tf-a']
-        variable_input_file = "projects/tf-a/variables.yaml"
-        with Pool() as p:
-            for device in devices:
-                tests = [(variable_input_file, device, testplan, project_device_path) for testplan in testplans]
-                results = p.map(call_lava_test_plan_testplans_project, tests)
-                self.assertTrue(all(results))
-        self.assertTrue(True)
 
-    def test_testplan_with_project_ti(self):
-        devices = [os.path.basename(d) for d in glob.glob("projects/ti/devices")]
-        testplans = ['ti-uboot']
-        variable_input_file = "projects/ti/variables.yaml"
-        with Pool() as p:
-            for device in devices:
-                tests = [(variable_input_file, device, testplan) for testplan in testplans]
-                results = p.map(call_lava_test_plan_testplans, tests)
-                self.assertTrue(all(results))
-        self.assertTrue(True)
+# ti tests
+ti_devices = ['x15-bl']
+assert len(ti_devices) > 0
+ti_testplans = ['ti-uboot']
+assert len(ti_testplans) > 0
+ti_variable_input_file = "projects/ti/variables.yaml"
+tests = []
+for device in ti_devices:
+    for testplan in ti_testplans:
+        tests.append((ti_variable_input_file, device, testplan))
+
+
+@pytest.mark.parametrize("param", tests)
+def test_call_lava_test_plan_testplans_ti(param):
+    variable_input_file, device, testplan = param
+    sys.argv = ["lava_test_plans", "--dry-run", "--variables", variable_input_file, "--device-type", device, "--test-plan", testplan, "--test-lava-validity"]
+    assert main() == 0, f"fail: {sys.argv}"
