71c1405bebd99a90b7d81699c729700c4d6be592
Anders Roxell <anders.roxell@linaro.org>
2022-12-02 01:06:42 +0100

    test: add tag tests
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

---
+++ test/test_lava_tags.py
@@ -0,0 +1,22 @@
+from lava_test_plans.__main__ import main
+
+from unittest import TestCase
+import sys
+import glob
+import os
+import pytest
+
+
+devices = ['hi960-hikey', 'x15', 'qemu_arm64']
+testcase = 'ltp-syscalls.yaml'
+variable_input_file = "test/variables-tags.ini"
+tests = []
+for device in devices:
+    tests.append((variable_input_file, device, testcase))
+
+
+@pytest.mark.parametrize("param", tests)
+def test_call_lava_tags_testcase(param):
+    variable_input_file, device, testcase = param
+    sys.argv = ["lava_test_plans", "--dry-run", "--variables", variable_input_file, "--device-type", device, "--test-case", testcase, "--test-lava-validity"]
+    assert main() == 0
