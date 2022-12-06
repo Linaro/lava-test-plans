291ecd192a1ac783a728a1bdfcdfb8f20cc123e1
Anders Roxell <anders.roxell@linaro.org>
2022-12-06 23:26:54 +0100

    test: test_lava_*: add a env var to skip '--test-lava-validity'
    Unit tests fails to run when it runs docker-in-docker or if its running
    in gitlab ci, docker-docker-docker the last docker is the python3-docker
    package.
    
    E       urllib3.exceptions.ProtocolError: ('Connection aborted.', FileNotFoundError(2, 'No such file or directory'))
    /usr/lib/python3/dist-packages/docker/transport/unixconn.py:43: ProtocolError
    During handling of the above exception, another exception occurred:
    
    Add a environment flag 'SKIP_TEST_LAVA_VALIDITY' if test-lava-validity
    shouldn't be tested.
    
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- test/test_lava_job_visibility.py
+++ test/test_lava_job_visibility.py
@@ -5,7 +5,9 @@ import sys
 import glob
 import os
 import pytest
+import shlex
 
+test_lava_validity = "" if os.getenv("SKIP_TEST_LAVA_VALIDITY") else "--test-lava-validity"
 
 devices = ['hi960-hikey', 'x15', 'qemu_arm64']
 testcase = 'ltp-syscalls.yaml'
@@ -18,5 +20,5 @@ for device in devices:
 @pytest.mark.parametrize("param", tests)
 def test_call_lava_visibility_group_testcase(param):
     variable_input_file, device, testcase = param
-    sys.argv = ["lava_test_plans", "--dry-run", "--variables", variable_input_file, "--device-type", device, "--test-case", testcase, "--test-lava-validity"]
+    sys.argv = shlex.split(f'lava_test_plans --dry-run --variables "{variable_input_file}" --device-type "{device}" --test-case "{testcase}" {test_lava_validity}')
     assert main() == 0
