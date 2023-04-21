728cc2316bc2829b3f52b43c858d667d478f84cf
Anders Roxell <anders.roxell@linaro.org>
2023-04-21 10:52:51 +0200

    test: add one tag test
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- test/test_lava_tags.py
+++ test/test_lava_tags.py
@@ -13,10 +13,11 @@ test_lava_validity = (
 
 devices = ["hi960-hikey", "x15", "qemu_arm64"]
 testcase = "ltp-syscalls.yaml"
-variable_input_files = ["test/variables-tags.ini"]
+variable_input_files = ["test/variables-tags.ini", "test/variables-tags-one-tag.ini"]
 tests = []
 for device in devices:
-    tests.append((variable_input_file, device, testcase))
+    for variable_file in variable_input_files:
+        tests.append((variable_file, device, testcase))
 
 
 @pytest.mark.parametrize("param", tests)
