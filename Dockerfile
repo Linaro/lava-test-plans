Linaro/lava-test-plans

commit ec703b5d0b884112ce404d8b8c53164438fafc4e

Dockerfile: update to python:3.11-slim-bookworm
Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9-slim-bullseye AS builder
+FROM python:3.11-slim-bookworm AS builder
 ARG DEBIAN_FRONTEND=noninteractive
 ARG version
 
@@ -13,7 +13,7 @@ RUN cd /lava-test-plans \
     && rm -rf .git \
     && FLIT_ALLOW_INVALID=1 flit build
 
-FROM python:3.9-slim-bullseye
+FROM python:3.11-slim-slim
 ARG DEBIAN_FRONTEND=noninteractive
 
 # hadolint ignore=DL3008

commit e372d5e7a5a0ea2f84064c73224acf71ee3e06ed

Dockerfile: fix container name
Fixes: ec703b5d0b88 ("Dockerfile: update to python:3.11-slim-bookworm")
Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

Dockerfile
@@ -13,7 +13,7 @@ RUN cd /lava-test-plans \
     && rm -rf .git \
     && FLIT_ALLOW_INVALID=1 flit build
 
-FROM python:3.11-slim-slim
+FROM python:3.11-slim-bookworm
 ARG DEBIAN_FRONTEND=noninteractive
 
 # hadolint ignore=DL3008